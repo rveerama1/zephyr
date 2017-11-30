1b61fc343d1ed62b9b77a0f47a4c7cefec7024c3
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-11-30 11:41:16 +0200

    samples: net: cc2520 experiment pinmux combinations
    If Ethernet and 802.15.4 both are enabled on FRDM_K64F board, then
    Tx and Rx pins are shared same (PTC16 and PTC17)  gpio pins. Which
    is blocking 802.15.4 traffic after exchange of few packets or it's
    always busy. Trying different combinations. So far no luck.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- samples/net/common/cc2520_frdm_k64f.c
+++ samples/net/common/cc2520_frdm_k64f.c
@@ -16,7 +16,109 @@
 #include <ieee802154/cc2520.h>
 #include <gpio.h>
 
-#define CC2520_GPIO_DEV_NAME CONFIG_GPIO_MCUX_PORTC_NAME
+#define TEST_1
+
+#if defined(TEST_1)
+
+#define CC2520_GPIO_PORTA CONFIG_GPIO_MCUX_PORTA_NAME
+#define CC2520_GPIO_PORTB CONFIG_GPIO_MCUX_PORTB_NAME
+#define CC2520_GPIO_PORTC CONFIG_GPIO_MCUX_PORTC_NAME
+
+#define CC2520_GPIO_VREG_EN	12  /* PTC12 */
+#define CC2520_GPIO_RESET	3   /* PTC3 */
+#define CC2520_GPIO_FIFO	2   /* PTC2 */
+#define CC2520_GPIO_CCA		1   /* PTA1 */
+#define CC2520_GPIO_SFD		9   /* PTB9 */
+#define CC2520_GPIO_FIFOP	4   /* PTC4 */
+
+static struct cc2520_gpio_configuration cc2520_gpios[CC2520_GPIO_IDX_MAX] = {
+	{ .dev = NULL, .pin = CC2520_GPIO_VREG_EN, },
+	{ .dev = NULL, .pin = CC2520_GPIO_RESET, },
+	{ .dev = NULL, .pin = CC2520_GPIO_FIFO, },
+	{ .dev = NULL, .pin = CC2520_GPIO_CCA, },
+	{ .dev = NULL, .pin = CC2520_GPIO_SFD, },
+	{ .dev = NULL, .pin = CC2520_GPIO_FIFOP, },
+};
+
+/* CONFIG_PINMUX_INIT_PRIORITY + 1 ! */
+#define CC2520_PINMUX_PRIORITY 46
+
+struct cc2520_gpio_configuration *cc2520_configure_gpios(void)
+{
+	struct device *gpioa;
+	struct device *gpiob;
+	struct device *gpioc;
+	int i;
+
+	gpioa = device_get_binding(CC2520_GPIO_PORTA);
+	if (!gpioa) {
+		return NULL;
+	}
+
+	gpiob = device_get_binding(CC2520_GPIO_PORTB);
+	if (!gpiob) {
+		return NULL;
+	}
+
+	gpioc = device_get_binding(CC2520_GPIO_PORTC);
+	if (!gpioc) {
+		return NULL;
+	}
+
+
+	for (i = 0; i < CC2520_GPIO_IDX_MAX; i++) {
+		struct device *gpio;
+		int flags;
+
+		if (i == CC2520_GPIO_IDX_SFD) {
+			gpio = gpiob;
+		} else if (i == CC2520_GPIO_IDX_CCA) {
+			gpio = gpioa;
+		} else {
+			gpio = gpioc;
+		}
+
+		if (i >= 0 && i < CC2520_GPIO_IDX_FIFO) {
+			flags = GPIO_DIR_OUT;
+		} else if (i < CC2520_GPIO_IDX_SFD) {
+			flags = GPIO_DIR_IN;
+		} else {
+			flags = (GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE |
+				 GPIO_INT_ACTIVE_HIGH | GPIO_INT_DEBOUNCE);
+		}
+
+		gpio_pin_configure(gpio, cc2520_gpios[i].pin, flags);
+		cc2520_gpios[i].dev = gpio;
+	}
+
+	return cc2520_gpios;
+}
+
+static int fdrm_k64f_cc2520_pinmux_setup(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	struct device *porta =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTA_NAME);
+
+	struct device *portb =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTB_NAME);
+
+	struct device *portc =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTC_NAME);
+
+	pinmux_pin_set(portc,  2, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portc,  3, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portc, 12, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portb,  9, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(porta,  1, PORT_PCR_MUX(kPORT_MuxAsGpio));
+
+	return 0;
+}
+
+#elif defined(TEST_2)
+#define CC2520_GPIO_PORTC CONFIG_GPIO_MCUX_PORTC_NAME
 
 #define CC2520_GPIO_VREG_EN	12  /* PTC12 */
 #define CC2520_GPIO_RESET	3   /* PTC3 */
@@ -39,11 +141,11 @@ static struct cc2520_gpio_configuration cc2520_gpios[CC2520_GPIO_IDX_MAX] = {
 
 struct cc2520_gpio_configuration *cc2520_configure_gpios(void)
 {
-	struct device *gpio;
+	struct device *gpioc;
 	int i;
 
-	gpio = device_get_binding(CC2520_GPIO_DEV_NAME);
-	if (!gpio) {
+	gpioc = device_get_binding(CC2520_GPIO_PORTC);
+	if (!gpioc) {
 		return NULL;
 	}
 
@@ -59,8 +161,8 @@ struct cc2520_gpio_configuration *cc2520_configure_gpios(void)
 				 GPIO_INT_ACTIVE_HIGH | GPIO_INT_DEBOUNCE);
 		}
 
-		gpio_pin_configure(gpio, cc2520_gpios[i].pin, flags);
-		cc2520_gpios[i].dev = gpio;
+		gpio_pin_configure(gpioc, cc2520_gpios[i].pin, flags);
+		cc2520_gpios[i].dev = gpioc;
 	}
 
 	return cc2520_gpios;
@@ -82,5 +184,8 @@ static int fdrm_k64f_cc2520_pinmux_setup(struct device *dev)
 
 	return 0;
 }
+
+
+#endif
 
 SYS_INIT(fdrm_k64f_cc2520_pinmux_setup, POST_KERNEL, CC2520_PINMUX_PRIORITY);

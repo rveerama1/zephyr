f0dd09ed6c112fe2c62fb376d919ed9ad83c7e5e
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-11-30 11:41:16 +0200

    (wip) cc2520_expr pinmux combinations
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- samples/net/common/cc2520_frdm_k64f.c
+++ samples/net/common/cc2520_frdm_k64f.c
@@ -16,13 +16,15 @@
 #include <ieee802154/cc2520.h>
 #include <gpio.h>
 
-#define CC2520_GPIO_DEV_NAME CONFIG_GPIO_MCUX_PORTC_NAME
+#define CC2520_GPIO_PORTA CONFIG_GPIO_MCUX_PORTA_NAME
+#define CC2520_GPIO_PORTB CONFIG_GPIO_MCUX_PORTB_NAME
+#define CC2520_GPIO_PORTC CONFIG_GPIO_MCUX_PORTC_NAME
 
 #define CC2520_GPIO_VREG_EN	12  /* PTC12 */
 #define CC2520_GPIO_RESET	3   /* PTC3 */
 #define CC2520_GPIO_FIFO	2   /* PTC2 */
-#define CC2520_GPIO_CCA		17  /* PTC17 */
-#define CC2520_GPIO_SFD		16  /* PTC16 */
+#define CC2520_GPIO_CCA		1   /* PTA1 */
+#define CC2520_GPIO_SFD		9   /* PTB9 */
 #define CC2520_GPIO_FIFOP	4   /* PTC4 */
 
 static struct cc2520_gpio_configuration cc2520_gpios[CC2520_GPIO_IDX_MAX] = {
@@ -39,16 +41,38 @@ static struct cc2520_gpio_configuration cc2520_gpios[CC2520_GPIO_IDX_MAX] = {
 
 struct cc2520_gpio_configuration *cc2520_configure_gpios(void)
 {
-	struct device *gpio;
+	struct device *gpioa;
+	struct device *gpiob;
+	struct device *gpioc;
 	int i;
 
-	gpio = device_get_binding(CC2520_GPIO_DEV_NAME);
-	if (!gpio) {
+	gpioa = device_get_binding(CC2520_GPIO_PORTA);
+	if (!gpioa) {
 		return NULL;
 	}
 
+	gpiob = device_get_binding(CC2520_GPIO_PORTB);
+	if (!gpiob) {
+		return NULL;
+	}
+
+	gpioc = device_get_binding(CC2520_GPIO_PORTC);
+	if (!gpioc) {
+		return NULL;
+	}
+
+
 	for (i = 0; i < CC2520_GPIO_IDX_MAX; i++) {
+		struct device *gpio;
 		int flags;
+
+		if (i == CC2520_GPIO_IDX_SFD) {
+			gpio = gpiob;
+		} else if (i == CC2520_GPIO_IDX_CCA) {
+			gpio = gpioa;
+		} else {
+			gpio = gpioc;
+		}
 
 		if (i >= 0 && i < CC2520_GPIO_IDX_FIFO) {
 			flags = GPIO_DIR_OUT;
@@ -70,6 +94,12 @@ static int fdrm_k64f_cc2520_pinmux_setup(struct device *dev)
 {
 	ARG_UNUSED(dev);
 
+	struct device *porta =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTA_NAME);
+
+	struct device *portb =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTB_NAME);
+
 	struct device *portc =
 		device_get_binding(CONFIG_PINMUX_MCUX_PORTC_NAME);
 
@@ -77,8 +107,8 @@ static int fdrm_k64f_cc2520_pinmux_setup(struct device *dev)
 	pinmux_pin_set(portc,  3, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(portc, 12, PORT_PCR_MUX(kPORT_MuxAsGpio));
-	pinmux_pin_set(portc, 16, PORT_PCR_MUX(kPORT_MuxAsGpio));
-	pinmux_pin_set(portc, 17, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portb,  9, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(porta,  1, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 	return 0;
 }

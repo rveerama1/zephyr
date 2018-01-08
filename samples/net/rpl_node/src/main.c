88445b1b66eb71843aaa5cd7bcbe615b013541c4
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2018-01-08 13:50:02 +0200

    samples: net: Add observer support to RPL node application
    If coap-client(e.g. RPL BR) wants to get notifications from
    coap-server(RPL-node) about parent status, client has to register
    observer for notifications.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- samples/net/rpl_node/src/main.c
+++ samples/net/rpl_node/src/main.c
@@ -26,6 +26,7 @@
 #include <icmpv6.h>
 #include <6lo.h>
 #include <ipv6.h>
+#include <rpl.h>
 
 #include <gpio.h>
 
@@ -72,6 +73,14 @@ static const char rpl_link[] = "Link Metric:";
 static const char rpl_no_link[] = "No link metric yet";
 
 static const char ipv6_no_nbr[] = "No IPv6 Neighbors";
+
+#define NUM_OBSERVERS 3
+#define NUM_PENDINGS 3
+
+static struct coap_observer observers[NUM_OBSERVERS];
+static struct coap_pending pendings[NUM_PENDINGS];
+
+static struct k_delayed_work retransmit_work;
 
 static void get_from_ip_addr(struct coap_packet *cpkt,
 			     struct sockaddr_in6 *from)
@@ -954,6 +963,185 @@ static int rpl_link_metric_get(struct coap_resource *resource,
 	return r;
 }
 
+static void retransmit_request(struct k_work *work)
+{
+	struct coap_pending *pending;
+	int r;
+
+	pending = coap_pending_next_to_expire(pendings, NUM_PENDINGS);
+	if (!pending) {
+		return;
+	}
+
+	r = net_context_sendto(pending->pkt, &pending->addr,
+			       sizeof(struct sockaddr_in6),
+			       NULL, 0, NULL, NULL);
+	if (r < 0) {
+		return;
+	}
+
+	if (!coap_pending_cycle(pending)) {
+		coap_pending_clear(pending);
+		return;
+	}
+
+	k_delayed_work_submit(&retransmit_work, pending->timeout);
+}
+
+static int append_rpl_parent(struct coap_packet *response)
+{
+	struct net_rpl_instance *rpl;
+	struct in6_addr *parent;
+	char out[RPL_MAX_REPLY];
+	u16_t out_len;
+	u8_t pos;
+
+	rpl = net_rpl_get_default_instance();
+
+	pos = snprintk(out, sizeof(out), "parent-");
+	out_len = pos;
+
+	if (!rpl || !rpl->current_dag || !rpl->current_dag->preferred_parent) {
+		pos = snprintk(&out[out_len], sizeof(out), "None");
+		out_len += pos;
+
+	} else {
+		parent = net_rpl_get_parent_addr(net_pkt_iface(response->pkt),
+					rpl->current_dag->preferred_parent);
+		pos = snprintk(&out[out_len], sizeof(out), "%s",
+			       net_sprint_ipv6_addr(parent));
+		out_len += pos;
+	}
+
+	pos = snprintk(&out[out_len], sizeof(out), "\nrank-");
+	out_len += pos;
+
+	if (!rpl || !rpl->current_dag) {
+		pos = snprintk(&out[out_len], sizeof(out), "None");
+		out_len += pos;
+	} else {
+		pos = snprintk(&out[out_len], sizeof(out), "%u",
+			       rpl->current_dag->rank);
+		out_len += pos;
+	}
+
+	return  coap_packet_append_payload(response, (u8_t *)out, out_len);
+}
+
+static int send_notification_packet(const struct sockaddr *addr,
+				    u16_t age,
+				    socklen_t addrlen,
+				    u16_t id,
+				    const u8_t *token,
+				    u8_t tkl,
+				    bool is_response)
+{
+	struct coap_packet response;
+	struct coap_pending *pending;
+	struct net_pkt *pkt;
+	struct net_buf *frag;
+	u8_t type = COAP_TYPE_CON;
+	int r;
+
+	if (is_response) {
+		type = COAP_TYPE_ACK;
+	}
+
+	if (!is_response) {
+		id = coap_next_id();
+	}
+
+	pkt = net_pkt_get_tx(context, K_FOREVER);
+	frag = net_pkt_get_data(context, K_FOREVER);
+
+	net_pkt_frag_add(pkt, frag);
+
+	r = coap_packet_init(&response, pkt, 1, type,
+			     tkl, (u8_t *)token,
+			     COAP_RESPONSE_CODE_CONTENT, id);
+	if (r < 0) {
+		net_pkt_unref(pkt);
+		return -EINVAL;
+	}
+
+	if (age >= 2) {
+		coap_append_option_int(&response, COAP_OPTION_OBSERVE, age);
+	}
+
+	r = coap_packet_append_payload_marker(&response);
+	if (r) {
+		net_pkt_unref(pkt);
+		return -EINVAL;
+	}
+
+	r = append_rpl_parent(&response);
+	if (r < 0) {
+		net_pkt_unref(pkt);
+		return -EINVAL;
+	}
+
+	if (type == COAP_TYPE_CON) {
+		pending = coap_pending_next_unused(pendings, NUM_PENDINGS);
+		if (!pending) {
+			return -EINVAL;
+		}
+
+		r = coap_pending_init(pending, &response, addr);
+		if (r) {
+			return -EINVAL;
+		}
+
+		coap_pending_cycle(pending);
+		pending = coap_pending_next_to_expire(pendings, NUM_PENDINGS);
+
+		k_delayed_work_submit(&retransmit_work, pending->timeout);
+	}
+
+	return net_context_sendto(pkt, addr, addrlen, NULL, 0, NULL, NULL);
+}
+
+/* RPL Observe Server */
+static int rpl_obs_get(struct coap_resource *resource,
+		       struct coap_packet *request)
+{
+	struct coap_observer *observer;
+	struct sockaddr_in6 from;
+	u8_t token[8];
+	u16_t id;
+	u8_t tkl;
+	bool observe = true;
+
+	get_from_ip_addr(request, &from);
+
+	if (!coap_request_is_observe(request)) {
+		observe = false;
+		goto done;
+	}
+
+	observer = coap_find_observer_by_addr(observers, NUM_OBSERVERS,
+					      (const struct sockaddr *)&from);
+	if (observer) {
+		goto done;
+	}
+
+	observer = coap_observer_next_unused(observers, NUM_OBSERVERS);
+	if (!observer) {
+		return -ENOMEM;
+	}
+
+	coap_observer_init(observer, request, (const struct sockaddr *)&from);
+	coap_register_observer(resource, observer);
+
+done:
+	id = coap_header_get_id(request);
+	tkl = coap_header_get_token(request, token);
+
+	return send_notification_packet((const struct sockaddr *)&from,
+					observe ? resource->age : 0,
+					sizeof(struct sockaddr_in6), id,
+					token, tkl, true);
+}
+
 static const char * const led_default_path[] = { "led", "0", NULL };
 static const char * const led_default_attributes[] = {
 	"title=\"LED0: led=toggle ?len=0..\"",
@@ -995,6 +1183,13 @@ static const char * const rpl_link_default_path[] = { "rpl-info",
 						      NULL };
 static const char * const rpl_link_default_attributes[] = {
 	"title=\"RPL Link Metric\"",
+	"rt=Text",
+	NULL };
+
+static const char * const rpl_obs_default_path[] = { "rpl-obs",
+						      NULL };
+static const char * const rpl_obs_default_attributes[] = {
+	"title=\"RPL Observe\"",
 	"rt=Text",
 	NULL };
 
@@ -1051,6 +1246,14 @@ static struct coap_resource resources[] = {
 	  .path = rpl_link_default_path,
 	  .user_data = &((struct coap_core_metadata) {
 			  .attributes = rpl_link_default_attributes,
+			}),
+	},
+	{ .get = rpl_obs_get,
+	  .post = NULL,
+	  .put = NULL,
+	  .path = rpl_obs_default_path,
+	  .user_data = &((struct coap_core_metadata) {
+			  .attributes = rpl_obs_default_attributes,
 			}),
 	},
 	{ },
@@ -1144,6 +1347,8 @@ static void init_app(void)
 	if (r) {
 		NET_ERR("Could not receive in the context");
 	}
+
+	k_delayed_work_init(&retransmit_work, retransmit_request);
 }
 
 void main(void)
